49d43ee0399d1d5c50fe0852a84e6a6759d16611
Yosept <yosept.flores@gmail.com>
2020-03-09 15:26:42 -0600

    We made the relationships of event and scopes

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,6 +1,6 @@
 class Invitation < ApplicationRecord
-  scope :accepted, where(is_accepted: true)
-  scope :pending, where(is_accepted: false)
+  scope :accepted, -> { where(is_accepted: true) }
+  scope :pending, -> { where(is_accepted: false) }
 
   belongs_to :invitor, class_name: 'User', foreign_key: :sent_by
   belongs_to :event
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  username: 'Joseph',
+  email: 'yosept@gmail.com',
+  password: '123lol',
+  password_confirmation: '123lol'
+)
+
+User.create(
+  username: 'Alfredo',
+  email: 'alfredo@gmail.com',
+  password: '123lol',
+  password_confirmation: '123lol'
+)
+
+User.create(
+  username: 'Benjamin',
+  email: 'benjamin@gmail.com',
+  password: '123lol',
+  password_confirmation: '123lol'
+)
+
+Event.create(
+  creator_id: User.first.id,
+  date: Time.now + 5.days,
+  name: 'the big party',
+  description: 'this is party so big, it describes itself'
+)
+
+Invitation.create(
+  user_id: User.third.id,
+  sent_by: User.second.id,
+  event_id: Event.first.id
+)

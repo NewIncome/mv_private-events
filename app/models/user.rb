e7543c566fee01676910f0433f3e04fed6c2585a
NewIncome <alfredo_cardenas@live.com>
2020-03-10 16:59:29 -0600

    Add intent of making a new association for previous and upcoming events.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :invitations
   has_many :attended_events, through: :invitations, source: :event
+  has_many :upcoming_events, through: :attended_events, source: :date
+  has_many :previously_events, -> { upcoming }, class_name: 'Invitation'
+
   has_many :accepted_invitations, -> { accepted }, class_name: 'Invitation'
   has_many :pending_invitations, -> { pending }, class_name: 'Invitation'
   has_many :created_invitations, foreign_key: :sent_by, class_name: 'Invitation'
